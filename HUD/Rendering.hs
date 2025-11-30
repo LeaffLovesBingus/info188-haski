7f83d4c8af021ec7e10c705d8d6a33c882f4aadc
cris-tobal <jcristobal2002@gmail.com>
2025-11-30 01:26:39 -0300

    Eliminado score

--- HUD/Rendering.hs
+++ HUD/Rendering.hs
@@ -7,7 +7,6 @@ import Graphics.Gloss
 import Types
 import HUD.Constants
 import HUD.HealthBar
-import HUD.Score
 import HUD.Inventory (drawInventory)
 
 
@@ -20,18 +19,13 @@ renderHUD gs =
       hpBarX = - (screenW / 2) + 100.0 + 50.0
       hpBarY = - (screenH / 2) + 60.0
 
-      -- Posición para el score (asumiendo top-right como el original)
       scoreX = screenW / 2 - 150.0
       scoreY = screenH / 2 - 50.0
 
-      -- Ítem
-      -- screenW / 2 es el borde derecho. Restamos un margen (ej. 60px)
-      -- -screenH / 2 es el borde inferior. Sumamos un margen (ej. 60px)
       itemX = screenW / 2 - 60.0
       itemY = - (screenH / 2) + 60.0
    in pictures
         [ translate (hpBarX - 120) hpBarY heartImage,
           translate hpBarX hpBarY $ drawHealthBar (playerHealth (player gs)),
-          translate scoreX scoreY $ drawScore 0,
           drawInventory (player gs)
         ]
